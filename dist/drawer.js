8592dc26695b0b9f61e47cef0f8b2d04ab4dbf5e
unpete <info@oknosoft.ru>
2021-02-11 23:59:32 +0500

    Импосты и раскладки во вложенном

--- dist/drawer.js
+++ dist/drawer.js
@@ -12087,35 +12087,30 @@ class ProfileNestedContent extends Profile {
     const h = bounds.height + bounds.y;
     const dir = new paper.Point(row.x2, h - row.y2).subtract(new paper.Point(row.x1, h - row.y1));
     let pelm;
-    let delta;
-    for(const elm of profiles) {
-      const {b, e, _row} = elm;
-      const pdir = e.subtract(b);
-      if(Math.abs(pdir.angle - dir.angle) < 0.1) {
-        row.path_data = _row.path_data;
-        const pt = new paper.Point((_row.x1 - row.x1 + _row.x2 - row.x2) / 2, (_row.y1 - row.y1 + _row.y2 - row.y2) / 2);
-        if(delta) {
-          delta = delta.add(pt).divide(2);
+    if(row.elm_type != 'Импост') {
+      for(const elm of profiles) {
+        const {b, e, _row} = elm;
+        const pdir = e.subtract(b);
+        if(Math.abs(pdir.angle - dir.angle) < 0.1) {
+          row.path_data = _row.path_data;
+          const pt = new paper.Point((_row.x1 - row.x1 + _row.x2 - row.x2) / 2, (_row.y1 - row.y1 + _row.y2 - row.y2) / 2);
+          row.x1 = _row.x1;
+          row.x2 = _row.x2;
+          row.y1 = _row.y1;
+          row.y2 = _row.y2;
+          pelm = elm;
+          break;
         }
-        else {
-          delta = pt;
-        }
-        row.x1 = _row.x1;
-        row.x2 = _row.x2;
-        row.y1 = _row.y1;
-        row.y2 = _row.y2;
-        pelm = elm;
-        break;
       }
     }
 
-    // if(!pelm) {
-    //   row.x1 += delta.x;
-    //   row.x2 += delta.x;
-    //   row.y1 += delta.y;
-    //   row.y2 += delta.y;
-    //   row.path_data = '';
-    // }
+    if(!pelm) {
+      row.x1 += layer.bounds.x;
+      row.x2 += layer.bounds.x;
+      row.y1 -= layer.bounds.y;
+      row.y2 -= layer.bounds.y;
+      row.path_data = '';
+    }
 
     super(attr);
     this._attr._nearest = pelm;
