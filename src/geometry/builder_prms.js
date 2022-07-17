964a5c2829e60b961a22d83bd3a2a4b90ff42a90
unpete <info@oknosoft.ru>
2022-07-17 21:01:43 +0300

    актуализация с веткой develop

---
+++ src/geometry/builder_prms.js
@@ -0,0 +1,104 @@
+
+/**
+ * Обёртка строки табчасти параметра
+ */
+class BuilderPrmRow {
+
+  constructor(_owner, _row) {
+    this._owner = _owner;
+    this._row = _row;
+  }
+
+  get row() {
+    return this._row.row;
+  }
+
+  get param() {
+    return this._row.param;
+  }
+
+  get inset() {
+    return this._row.inset;
+  }
+
+  get sorting_field() {
+    return this.param.sorting_field;
+  }
+
+  get value() {
+    return this._row.value;
+  }
+  set value(value) {
+    if(this.value == value) {
+      return;
+    }
+    const {inset, param, _owner, _row} = this;
+    const {params, cnstr} = _owner;
+    if(_row.cnstr) {
+      const prow = params.find({cnstr: 0, param, inset});
+      if(prow?.value == value) {
+        params.del(_row);
+        this._row = prow;
+      }
+      else {
+        _row.value = value;
+      }
+    }
+    else {
+      this._row = params.add({cnstr, param, inset, value});
+    }
+  }
+}
+
+/**
+ * Свойства слоя или элемента
+ *
+ * Created by Evgeniy Malyarov on 11.04.2022.
+ */
+class BuilderPrms {
+
+  constructor({elm, layer}) {
+    this.elm = elm;
+    this.layer = layer;
+    this.cnstr = layer ? layer.cnstr : elm.elm;
+  }
+
+  get _name() {
+    return 'params';
+  }
+
+  get _owner() {
+    return this.elm ? (elm.prm_ox || elm.ox) : this.layer._ox;
+  }
+
+  get params() {
+    return this._owner.params;
+  }
+
+  find_rows({inset}, cb) {
+    const {cnstr, params, layer: {sys, own_sys}} = this;
+    const map = new Map();
+    const cnstrs = [0, cnstr];
+    for(const row of params) {
+      if(cnstrs.includes(row.cnstr) && row.inset == inset && !row.hide) {
+        if(own_sys && !sys.product_params.find({param: row.param})) {
+          continue;
+        }
+        if(!map.has(row.param) || row.cnstr) {
+          map.set(row.param, row);
+        }
+      }
+    }
+    const res = [];
+    for(const [param, row] of map) {
+      res.push(new BuilderPrmRow(this, row));
+    }
+
+    res.sort($p.utils.sort('sorting_field'));
+    for(const row of res) {
+      cb(row);
+    }
+    return res;
+  }
+
+}
---
+++ src/modifiers/enums/enm_count_calculating_ways.js
@@ -0,0 +1,108 @@
+/**
+ * Дополнительные методы перечисления 'Способы расчёта количества'
+ *
+ * @module enm_count_calculating_ways
+ *
+ * Created 01.05.2022.
+ */
+
+(function({enm, cat: {clrs}, cch}){
+
+  const {coloring, len_prm, area} = enm.count_calculating_ways;
+  const {new_spec_row, calc_qty_len, calc_count_area_mass} = ProductsBuilding;
+
+  const is_side = (side) => ['_in', '_out'].includes(side);
+
+  coloring.calculate = function ({inset, elm, row_spec, row_ins_spec, spec, ox}) {
+    let {_clr, _clr_side, quantity, sz, coefficient, angle_calc_method, formula} = row_ins_spec;
+    if(!_clr) {
+      _clr = elm.clr;
+    }
+    const prefix = _clr.area_src.valueOf();
+    if(prefix) {
+      const {_row} = elm;
+      const nom = elm.inset === inset ? elm.nom : inset.nom(elm);
+      row_spec.clr = clrs.by_predefined(row_ins_spec.clr, _clr, ox.clr, elm, spec);
+
+      if(is_side(_clr_side)) {
+        row_spec.width = nom._extra(prefix + _clr_side);
+      }
+      else {
+        const areas = [nom._extra(prefix) || 0, nom._extra(prefix + '_in') || 0, nom._extra(prefix + '_out') || 0];
+        row_spec.width = areas[0] || (areas[1] + areas[2]);
+      }
+      if(row_spec.width) {
+        row_spec.qty = quantity;
+        row_spec.len = (elm.length / 1000).round(3);
+        row_spec.s = row_spec.len * row_spec.width * (coefficient || 1);
+      }
+    }
+    if(!row_spec.width) {
+      row_spec.qty = 0;
+    }
+    return row_spec;
+  };
+
+  area.calculate = function ({inset, elm, row_spec, row_ins_spec}) {
+    const {_row} = elm;
+    const {quantity, sz, coefficient} = row_ins_spec;
+    row_spec.qty = quantity;
+    if(inset.insert_type == enm.inserts_types.mosquito) {
+      const bounds = elm.layer.bounds_inner(sz);
+      row_spec.len = bounds.height * coefficient;
+      row_spec.width = bounds.width * coefficient;
+      row_spec.s = (row_spec.len * row_spec.width).round(4);
+    }
+    else if(inset.insert_type == enm.inserts_types.jalousie) {
+      if(elm.bounds_light) {
+        const bounds = elm.bounds_light();
+        row_spec.len = (bounds.height + offsets) * coefficient;
+        row_spec.width = (bounds.width + sz) * coefficient;
+      }
+      else {
+        row_spec.len = elm.len * coefficient;
+        row_spec.width = elm.height * coefficient;
+      }
+      row_spec.s = (row_spec.len * row_spec.width).round(4);
+    }
+    else if(inset.insert_type == enm.inserts_types.product) {
+      const {project} = elm;
+      const {width, height} = project.bounds;
+      row_spec.len = width / 1000;
+      row_spec.width = height / 1000;
+      row_spec.s = (project.form_area - sz) * coefficient;
+    }
+    else if(inset.insert_type == enm.inserts_types.layer) {
+      const {layer} = elm;
+      const {width, height} = layer.bounds;
+      row_spec.len = width / 1000;
+      row_spec.width = height / 1000;
+      row_spec.s = (layer.form_area - sz) * coefficient;
+    }
+    else {
+      row_spec.len = (_row.y2 - _row.y1 - sz) * coefficient;
+      row_spec.width = (_row.x2 - _row.x1 - sz) * coefficient;
+      row_spec.s = _row.s;
+    }
+    return row_spec;
+  };
+
+  len_prm.calculate = function ({inset, elm, row_spec, row_ins_spec, origin}) {
+    let len = 0;
+
+    inset.selection_params.find_rows({elm: row_ins_spec.elm}, (prm_row) => {
+      const {param} = prm_row;
+      if(param.type.digits) {
+        len = elm.layer.extract_pvalue({param, cnstr: 0, elm, origin, prm_row})
+      }
+      if(len) return false;
+    });
+    const {quantity, sz, coefficient} = row_ins_spec;
+    row_spec.qty = quantity;
+    row_spec.len = len ? (len - sz) * coefficient : 0;
+    row_spec.width = 0;
+    row_spec.s = 0;
+    return row_spec;
+  };
+
+})($p);
