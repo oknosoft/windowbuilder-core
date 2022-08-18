86578420925513f5ddb81c86dad1c5d3d9aab759
unpete <info@oknosoft.ru>
2022-08-18 20:31:57 +0300

    размер артикула в номенклатуре увеличен до 32 символов

--- src/modifiers/chartscharacteristics/predefined.js
+++ src/modifiers/chartscharacteristics/predefined.js
@@ -17,17 +17,18 @@ $p.adapters.pouch.once('pouch_doc_ram_loaded', () => {
       if(prm.calculated.empty()) {
         prm.calculated = formulas.create({ref: prm.ref, name: `predefined-${name}`}, false, true);
       }
-      if(!prm.calculated._data._formula) {
+      const {_data} = prm.calculated;
+      if(!_data._formula) {
         switch (name) {
 
         case 'clr_product':
-          prm.calculated._data._formula = function (obj) {
+          _data._formula = function (obj) {
             return obj?.ox?.clr || clrs.get();
           };
           break;
 
         case 'clr_inset':
-          prm.calculated._data._formula = function ({elm, cnstr, ox}) {
+          _data._formula = function ({elm, cnstr, ox}) {
             let clr;
             if(elm instanceof $p.DpBuyers_orderProductionRow || elm instanceof $p.DocCalc_order.FakeElm) {
               clr = elm.clr;
@@ -40,13 +41,13 @@ $p.adapters.pouch.once('pouch_doc_ram_loaded', () => {
           break;
 
         case 'width':
-          prm.calculated._data._formula = function (obj) {
+          _data._formula = function (obj) {
             return obj?.ox?.y || 0;
           };
           break;
 
         case 'inset':
-          prm.calculated._data._formula = function ({elm, prm_row, ox, row}) {
+          _data._formula = function ({elm, prm_row, ox, row}) {
 
             // если запросили вставку соседнего элемента состава заполнения, возвращаем массив
             if(prm_row && prm_row.origin === prm_row.origin._manager.nearest && elm instanceof EditorInvisible.Filling){
@@ -64,33 +65,33 @@ $p.adapters.pouch.once('pouch_doc_ram_loaded', () => {
           break;
 
         case 'elm_weight':
-          prm.calculated._data._formula = function (obj) {
+          _data._formula = function (obj) {
             const {elm} = obj || {};
             return elm ? elm.weight : 0;
           };
           break;
 
         case 'elm_orientation':
-          prm.calculated._data._formula = function ({elm, elm2}) {
+          _data._formula = function ({elm, elm2}) {
             return elm?.orientation || elm2?.orientation || orientations.get();
           };
           break;
 
         case 'elm_pos':
-          prm.calculated._data._formula = function ({elm}) {
+          _data._formula = function ({elm}) {
             return elm?.pos || positions.get();
           };
           break;
 
         case 'elm_rectangular':
-          prm.calculated._data._formula = function ({elm}) {
+          _data._formula = function ({elm}) {
             const {is_rectangular} = elm;
             return typeof is_rectangular === 'boolean' ? is_rectangular : true;
           };
           break;
 
         case 'branch':
-          prm.calculated._data._formula = function ({elm, layer, ox, calc_order}) {
+          _data._formula = function ({elm, layer, ox, calc_order}) {
             if(!calc_order && ox) {
               calc_order = ox.calc_order;
             }
@@ -107,7 +108,7 @@ $p.adapters.pouch.once('pouch_doc_ram_loaded', () => {
           break;
 
         default:
-          prm.calculated._data._formula = function () {};
+          _data._formula = function () {};
         }
       }
     }
