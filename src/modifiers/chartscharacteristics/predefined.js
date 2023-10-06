899c3aa28a1fe4421b500efe5e69bca63b34584f
unpete <info@oknosoft.ru>
2023-10-07 00:10:44 +0500

    системный параметр "есть ось поворота"

--- src/modifiers/chartscharacteristics/predefined.js
+++ src/modifiers/chartscharacteristics/predefined.js
@@ -136,7 +136,24 @@ $p.adapters.pouch.once('pouch_doc_ram_loaded', () => {
 
           case 'rotation_axis':
             _data._formula = function ({elm, layer, prm_row, ox, cnstr}) {
-              return Bollean((layer || elm?.layer)?.furn?.open_tunes.find({rotation_axis: true}));
+              if(!layer && elm?.layer) {
+                layer = elm?.layer;
+              }
+              if(!layer) {
+                return false;
+              }
+              if(prm_row.origin.is('layer') || prm_row.origin.is('nearest')) {
+                return Boolean(layer.furn.open_tunes.find({rotation_axis: true})); 
+              }
+              let res = false;
+              layer.furn.open_tunes.find_rows({rotation_axis: true}, ({side}) => {
+                const profile = layer.profiles_by_side(side);
+                if(profile === elm) {
+                  res = true;
+                  return false;
+                }
+              });
+              return res;
             };
             break;
 
