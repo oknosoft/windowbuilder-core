52f5cc7d2e1cc2e5f15644a1cc438a25724fa94a
unpete <info@oknosoft.ru>
2024-02-15 19:15:41 +0500

    расширение CatInserts

--- src/catalogs/inserts/classes.js
+++ src/catalogs/inserts/classes.js
@@ -2,23 +2,19 @@
 export const exclude = [/*'cat.inserts'*/];
 
 export function classes({cat, classes, symbols}, exclude)  {
-  const {CatObj} = classes;
+
   const {get, set} = symbols;
 
-  /*
-  
-class CatInserts extends CatObj{
-    get insert_type(){return this[get]('insert_type')}
-    set insert_type(v){this[set]('insert_type',v)}
-    get insert_glass_type(){return this[get]('insert_glass_type')}
-    set insert_glass_type(v){this[set]('insert_glass_type',v)}
-
+  class CatInserts extends classes.CatInserts {
     nom(elm) {
-      return 'nom';
+      return cat.nom.get();
     }
   }
-  classes.CatInserts = CatInserts;
 
+  classes.CatInserts = CatInserts;
+  
+  /*
+  
   cat.create('inserts');
      
   */
