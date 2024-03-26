d3a34927505297a2d4af514acb7f3830a54e4bd2
unpete <info@oknosoft.ru>
2024-03-26 23:27:13 +0500

    доступные вставки рядов профиля

--- src/metadata/catalogs/cat_production_params.js
+++ src/metadata/catalogs/cat_production_params.js
@@ -135,19 +135,10 @@ exports.CatProduction_params = class CatProduction_params extends Object {
     else if(typeof elm_types == 'string') {
       elm_types = types[elm_types];
     }
-    else if(!Array.isArray(elm_types)) {
-      if(elm && elm_types === types.region) {
-        const nearest = elm.nearest();
-        if(nearest) {
-          elm_types = nearest.elm_type;
-        }
-        else if(!elm.is_t && elm.layer.layer) {
-          elm_types = types.flap;
-        }
-        else {
-          elm_types = types.rama_impost;
-        }
-      }
+    if(elm && elm_types === types.region) {
+      return this.region_inserts(rows, elm);
+    }
+    if(!Array.isArray(elm_types)) {
       elm_types = [elm_types];
     }
 
@@ -197,6 +188,34 @@ exports.CatProduction_params = class CatProduction_params extends Object {
     });
 
     return noms.map((e) => e.nom);
+  }
+  
+  region_inserts(rows, elm) {
+    let elm_types;
+    const nearest = elm.nearest();
+    if(nearest) {
+      elm_types = nearest.elm_type;
+    }
+    else if(!elm.is_t && elm.layer.layer) {
+      elm_types = types.flap;
+    }
+    else {
+      elm_types = types.rama_impost;
+    }
+    const inserts = this.inserts(elm_types, rows, nearest);
+    if(rows === 'rows') {
+      return inserts;
+    }
+    const res = new Set();
+    const {region} = elm.layer;
+    for(const inset of inserts) {
+      for(const row of inset.inserts) {
+        if(row.inset.region === region) {
+          res.add(row.inset);
+        }
+      }
+    }
+    return Array.from(res);
   }
 
   /**
