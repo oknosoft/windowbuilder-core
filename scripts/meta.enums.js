b9754aba493e6074d3339843de62684a7fa49f28
unpete <info@oknosoft.ru>
2024-06-01 19:23:26 +0500

    rebase master

--- scripts/meta.enums.js
+++ scripts/meta.enums.js
@@ -66,6 +66,8 @@ const custom_constructor = [
   'CatUsersExtra_fieldsRow',
   'CatProduction_paramsExtra_fieldsRow',
   'CatParameters_keysParamsRow',
+  'CatCharacteristicsCoordinatesRow',
+  'CatCharacteristicsInsertsRow',
 ];
 
 // конструктор metadata-core и плагин metadata-pouchdb
