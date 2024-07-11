6a8107e666a0bc29e93d900c723b8dc6e79ea4b1
unpete <info@oknosoft.ru>
2024-07-11 14:37:07 +0300

    Degree для поворота

--- src/geometry/Three.js
+++ src/geometry/Three.js
@@ -1,4 +1,37 @@
-import {Vector3, Quaternion} from 'three';
+import {Vector3, Quaternion, MathUtils} from 'three';
+
+const {degToRad, radToDeg} = MathUtils;
+
+class Degree {
+  constructor(owner) {
+    this.owner = owner;
+  }
+
+  fromArray(v) {
+    this.owner.rotation.fromArray(v.map(degToRad));
+  }
+  
+  get x() {
+    return radToDeg(this.owner.rotation.x);
+  }
+  set x(v) {
+    this.owner.rotation.x = degToRad(v);
+  }
+
+  get y() {
+    return radToDeg(this.owner.rotation.y);
+  }
+  set y(v) {
+    this.owner.rotation.y = degToRad(v);
+  }
+
+  get z() {
+    return radToDeg(this.owner.rotation.z);
+  }
+  set z(v) {
+    this.owner.rotation.z = degToRad(v);
+  }
+}
 
 export class Three {
 
@@ -8,22 +41,20 @@ export class Three {
     Object.assign(this.#raw, {
       position: new Vector3(),
       rotation: new Vector3(),
-      quaternion: new Quaternion(),
+      degree: new Degree(this),
     });
     if(raw) {
       this.fill(raw);
     }
+    
   }
   
-  fill({position, rotation, quaternion}) {
+  fill({position, rotation}) {
     if(position) {
       this.#raw.position.fromArray(position);
     }
     if(rotation) {
       this.#raw.rotation.fromArray(rotation);
-    }
-    if(quaternion) {
-      this.#raw.quaternion.fromArray(quaternion);
     }
   }
   
@@ -31,7 +62,7 @@ export class Three {
     return this.#raw.position;
   }
   set position(v) {
-    if(array.isArray(v)) {
+    if(Array.isArray(v)) {
       this.#raw.position.fromArray(v);
     }
     else {
@@ -39,27 +70,27 @@ export class Three {
     }
   }
 
-  get quaternion() {
-    return this.#raw.quaternion;
-  }
-  set quaternion(v) {
-    if(array.isArray(v)) {
-      this.#raw.quaternion.fromArray(v);
-    }
-    else {
-      Object.assign(this.#raw.quaternion, v);
-    }
-  }
-
   get rotation() {
     return this.#raw.rotation;
   }
   set rotation(v) {
-    if(array.isArray(v)) {
+    if(Array.isArray(v)) {
       this.#raw.rotation.fromArray(v);
     }
     else {
       Object.assign(this.#raw.rotation, v);
+    }
+  }
+
+  get degree() {
+    return this.#raw.degree;
+  }
+  set degree(v) {
+    if(Array.isArray(v)) {
+      this.#raw.degree.fromArray(v);
+    }
+    else {
+      Object.assign(this.#raw.degree, v);
     }
   }
   
