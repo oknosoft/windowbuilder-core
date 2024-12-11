3d1581ebeb6bf390d87fa8b8da3a3aff317e6f6a
unpete <info@oknosoft.ru>
2024-12-11 22:14:28 +0500

    размер соединения в пути заполнения

--- src/geometry/Filling.js
+++ src/geometry/Filling.js
@@ -7,9 +7,24 @@ export class Filling extends ContainerBlank {
     return this.children.path;
   }
   set path(outer) {
-    const {path} = this;
+    const {path, project: {root}} = this;
+    const ribs = outer.map((src, index) => {
+      const {edge} = src;
+      const curr = src.clone();
+      const next = (index === outer.length - 1 ? outer[0] : outer[index + 1]).clone();
+      const cnns = root.cat.cnns.iiCnns(this, edge.profile).filter(v => v.art1glass);
+      const size = cnns.length ? cnns[0].size(this, edge.profile) : 0;
+      const rib = new paper.Path({insert: false, segments: [curr, next]})
+        .equidistant(size, (size + 20) * 2);
+      return rib;
+    });
+    
     path.removeSegments();
-    path.addSegments(outer.map(v => v.clone()));
+    path.addSegments(ribs.map((curr, index) => {
+      const prev = (index === 0 ? ribs[outer.length - 1] : ribs[index - 1]).clone();
+      const pt = curr.intersectPoint(prev);
+      return pt;
+    }));
     path.closePath();
     const {bounds} = path;
     path.fillColor = new paper.Color({
