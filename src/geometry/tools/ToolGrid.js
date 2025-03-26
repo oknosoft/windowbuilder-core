bbec5875cb2344ab94fc0fe73738e987cabf2c08
unpete <info@oknosoft.ru>
2025-03-26 14:04:25 +0300

    сетка витража

--- src/geometry/tools/ToolGrid.js
+++ src/geometry/tools/ToolGrid.js
@@ -1,5 +1,6 @@
 import paper from 'paper/dist/paper-core';
 import {ToolElement} from './ToolElement';
+import {DimensionLine} from '../DimensionLine';
 
 const {Point} = paper;
 
@@ -11,8 +12,8 @@ export class ToolGrid extends ToolElement {
       name: 'grid',
       dp: $p.dp.builderGrid.create({
         split: 'ДелениеГоризонтальных',
-        w: 6000,
-        h: 6000,
+        w: 5000,
+        h: 5000,
         align_by_x: 'left',
         align_by_y: 'bottom',
       }),
@@ -38,7 +39,7 @@ export class ToolGrid extends ToolElement {
   }
 
   mouseup(event) {
-    const {view} = this._scope;
+    this.createProfiles();
   }
 
   keydown(event) {
@@ -57,6 +58,64 @@ export class ToolGrid extends ToolElement {
       case 'down':
         rootLayer.move(new Point(0, 10));
         break;
+    }
+  }
+
+  createProfiles() {
+    const {project, dp: {h, align_by_x, align_by_y, sizes}} = this;
+    const {activeLayer} = project;
+    activeLayer.clear(true);
+    if(!h) {
+      return;
+    }
+    // стойки
+    const byX = sizes.filter(row => row.elm === 1 && row.sz > 0);
+    if(byX.length) {
+      const profiles = [];
+      // стойки
+      const xMap = new Map();
+      let x = 0;
+      for(let i = 0; i <= byX.length; i++) {
+        if(i) {
+          x += byX[i - 1].sz;
+        }
+        const attr = i ? {b: [x, h], e: [x, 0]} : {e: [x, h], b: [x, 0]};
+        const profile = activeLayer.createProfile(attr);
+        profiles.push(profile);
+        xMap.set(x, profile);
+
+        if(i) {
+          new DimensionLine({
+            project,
+            owner: activeLayer,
+            parent: project.dimensions,
+            elm1: profiles[i],
+            elm2: profiles[i - 1],
+            p1: 'e',
+            p2: i ? 'e' : 'b',
+            pos: 'bottom',
+            offset: -280,
+          });
+        }
+      }
+      activeLayer.skeleton.addProfiles(profiles);
+      if(profiles.length > 2) {
+        new DimensionLine({
+          project,
+          owner: activeLayer,
+          parent: project.dimensions,
+          elm1: profiles[profiles.length - 1],
+          elm2: profiles[0],
+          p1: 'e',
+          p2: 'b',
+          pos: 'bottom',
+          offset: -500,
+        });
+      }
+      profiles.length = 0;
+
+      project.redraw();
+      project.zoomFit();
     }
   }
 }
