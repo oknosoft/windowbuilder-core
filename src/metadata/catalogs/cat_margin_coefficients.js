77a50a6d708b969e1d9240b8a32b7803566dc22f
unpete <info@oknosoft.ru>
2025-07-02 21:16:40 +0300

    вытеснение в МК2

--- src/metadata/catalogs/cat_margin_coefficients.js
+++ src/metadata/catalogs/cat_margin_coefficients.js
@@ -112,8 +112,8 @@ exports.CatMargin_coefficientsManager = class CatMargin_coefficientsManager exte
             obj = origin;
           }
           else {
-            const {nom} = _owner.calc_order_row;
-            const map = this.nom_groups[nom.ref] || this.nom_groups[nom.nom_group.ref];
+            const {owner} = _owner;
+            const map = this.nom_groups[owner.ref] || this.nom_groups[owner.nom_group.ref];
             if(map) {
               obj = map;
             }
