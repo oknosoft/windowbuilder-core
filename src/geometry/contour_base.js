99877d3051fc3d15ce445ac9482a3e06ebffd1b3
unpete <info@oknosoft.ru>
2025-09-01 19:55:50 +0300

    прочищаем возможных паразитов

--- src/geometry/contour_base.js
+++ src/geometry/contour_base.js
@@ -2647,19 +2647,6 @@ class Contour extends AbstractFilling(paper.Layer) {
     
     // сюда добавляли прочистку паразитов, но убрали
 
-    // прочищаем возможных паразитов
-    const rm = [];
-    const {Group} = paper;
-    for(const item of this.children) {
-      if(item instanceof Group) {
-        continue;
-      }
-      rm.push(item);
-    }
-    for(const item of rm) {
-      item.remove?.();
-    }
-
     // информируем мир о новых размерах нашего контура
     this.notify(this, 'contour_redrawed', this._attr._bounds);
 
