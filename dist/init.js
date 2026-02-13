a34acd42ee054a9b8dc75dd08721d594a97a0645
unpete <info@oknosoft.ru>
2026-02-14 01:03:56 +0500

    договор в заказе поставщику

--- dist/init.js
+++ dist/init.js
@@ -7768,6 +7768,24 @@ get goods(){return this._getter_ts('goods')}
 set goods(v){this._setter_ts('goods',v)}
 
 
+  value_change(field, type, value) {
+    const ads = [];
+    const {enm: {contract_kinds: {СПоставщиком}}, cat: {contracts}} = $p;
+    if(field === 'organization') {
+      this.organization = value;
+      if(this.contract.organization != value) {
+        this.contract = contracts.by_partner_and_org(this.partner, value, СПоставщиком);
+        !this.constructor.prototype.hasOwnProperty('new_number_doc') && this.new_number_doc();
+        ads.push('contract');
+      }
+    }
+    else if(field === 'partner' && this.contract.owner != value) {
+      this.contract = contracts.by_partner_and_org(value, this.organization, СПоставщиком);
+      ads.push('contract');
+    }
+    ads.length && this._manager.emit_add_fields(this, ads);
+  }
+
   save(...attr) {
     const basisRow = this.basis.orders.find({invoice: this});
     if(basisRow) {
